4f3586f40a2a8a69f4b442783b777d4bcb1fc3f3
engrashid <rashid.rob@hotmail.com>
2018-11-25 20:40:25 +0600

    Bug fix

--- src/android/PrintWrapper.java
+++ src/android/PrintWrapper.java
@@ -266,7 +266,7 @@ public class PrintWrapper extends CordovaPlugin implements PrinterObserver{
                         escCmd.append(escCmd.getLFCRCmd());//回车换行
 
                         textSetting.setCpclFontSize(200);
-                        textSetting.setIsEscSmallCharactor(SettingEnum.Enable);
+                        textSetting.setIsEscSmallCharactor(SettingEnum.Disable);
                         textSetting.setBold(SettingEnum.Disable);
                         textSetting.setDoubleHeight(SettingEnum.Disable);
                         textSetting.setDoubleWidth(SettingEnum.Disable);
